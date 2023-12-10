009c9922e6d59d236f9555858cd68952a7ef4c67
Costache Ioana-Brandusa-Alexandra <costachealexandra28@gmail.com>
2023-12-10 23:08:54 +0200

    Implemented tests

--- test_app.py
+++ test_app.py
@@ -23,6 +23,32 @@ class TestApp(unittest.TestCase):
             response = self.app.get('/reports', content_type='application/json')
             self.assertEqual(response.status_code, 200)
 
+        taskCreate = {
+            "userId": None,
+            "name": "Test",
+            "description": "idk",
+            "status": None,
+            "finish": None
+        }
+
+        with self.app.session_transaction() as session:
+            session['user_id'] = 5
+            response = self.app.post('/task',data=json.dumps(taskCreate), content_type='application/json')
+            self.assertEqual(response.status_code, 200)
+
+        taskUpdate = {
+            "userId": None,
+            "name": "Test1",
+            "description": "idk2",
+            "status": None,
+            "finish": None
+        }
+
+        with self.app.session_transaction() as session:
+            session['user_id'] = 5
+            response = self.app.patch('/task/1', data=json.dumps(taskUpdate), content_type='application/json')
+            self.assertEqual(response.status_code, 200)
+
     def test_employee(self):
         data = {"email": "employeenumber1@company.com",
                 "password": "employee1"}
